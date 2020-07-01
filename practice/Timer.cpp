a769e2c0237b21e5f8ee39a1a1ab238d3a97e866
author123 <hipas1985@gmail.com>
2020-07-01 18:05:02 +0300

    test commit

---
+++ practice/Timer.cpp
@@ -0,0 +1,29 @@
+#include "Timer.h"
+
+Stopwatch& Stopwatch::reset()
+{
+	duration_ = std::chrono::nanoseconds(0);
+	active = false;
+	return *this;
+}
+
+Stopwatch& Stopwatch::start()
+{
+	if (!active)
+	{
+		start_ = Clock::now();
+		active = true;
+	}
+	return *this;
+}
+
+Stopwatch& Stopwatch::stop()
+{
+	if (active)
+	{
+		stop_ = Clock::now();
+		duration_ += stop_ - start_;
+		active = false;
+	}
+	return *this;
+}
--- practice/Timer.h
+++ practice/Timer.h
@@ -1,49 +1,26 @@
 #pragma once
-#pragma once
 #include <iostream>    // printf()
 #include <chrono>
 using namespace std;
 
-class muTimer
+class Stopwatch
 {
 	using Clock = std::chrono::high_resolution_clock;
 	bool active = false;
 	Clock::duration   duration_;
 	Clock::time_point start_ = Clock::now(), stop_ = Clock::now();
 
-	muTimer(const muTimer&) = delete;
-	muTimer& operator=(const muTimer&) = delete;
+	Stopwatch(const Stopwatch&) = delete;
+	Stopwatch& operator=(const Stopwatch&) = delete;
 public:
 	using ns = std::chrono::nanoseconds;
 	using mks = std::chrono::microseconds;
 	using ms = std::chrono::milliseconds;
-	muTimer() { reset(); start(); }
-	~muTimer() = default;
-	muTimer& reset()
-	{
-		duration_ = std::chrono::nanoseconds(0);
-		active = false;
-		return *this;
-	}
-	muTimer& start()
-	{
-		if (!active)
-		{
-			start_ = Clock::now();
-			active = true;
-		}
-		return *this;
-	}
-	muTimer& stop()
-	{
-		if (active)
-		{
-			stop_ = Clock::now();
-			duration_ += stop_ - start_;
-			active = false;
-		}
-		return *this;
-	}
+	Stopwatch() { reset(); start(); }
+	~Stopwatch() = default;
+	Stopwatch& reset();
+	Stopwatch& start();
+	Stopwatch& stop();
 	template<typename T = mks>
 	unsigned long long duration()
 	{
